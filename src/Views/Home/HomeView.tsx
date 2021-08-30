3239a3e5bec5c67e27a2c72fb32f96d6d566d47d
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-08-31 05:58:38 +0800

    home view intial

---
+++ src/Views/Home/HomeView.tsx
@@ -0,0 +1,11 @@
+import React,{FC} from 'react'
+import background from "../../assets/images/background.jpg"
+const HomeView:FC=()=> {
+    return (
+        <div>
+            <img src={background} alt={"background"}></img>
+        </div>
+    )
+}
+
+export default HomeView
