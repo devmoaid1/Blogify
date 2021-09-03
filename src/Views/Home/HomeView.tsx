06657954ecb16a51e2256b4356c449c43c5ae5b8
devmoaid1 <68110578+devmoaid1@users.noreply.github.com>
2021-09-03 08:01:39 +0800

    landing page

--- src/Views/Home/HomeView.tsx
+++ src/Views/Home/HomeView.tsx
@@ -1,11 +1,38 @@
 import React,{FC} from 'react'
-import background from "../../assets/images/background.jpg"
-import "./home.css"
+import background from "../../assets/images/background2.png"
+import { Text,
+     Box  ,Flex,Heading,Spacer,Image} from "@chakra-ui/react" 
 const HomeView:FC=()=> {
     return (
-        <div className="HomeContainer">
-            <img className="backgroundimage" src={background} alt={"background"}></img>
-        </div>
+        <Box as="div" height="100vh" bg="gray.100" position="relative">
+              
+              <Flex p="4" postion="absloute" top='0'>
+  <Box ml="4">
+    <Heading size="xl" fontFamily="saira" color="primary.100">Blogify</Heading>
+  </Box>
+  <Spacer />
+  <Box mr="10">
+    <Box as="a" cursor="pointer" color="primary.100" mr="4">
+      Sign Up
+    </Box>
+    <Box as="a" cursor="pointer" color="primary.100">Log in</Box>
+  </Box>
+</Flex> 
+
+         <Box height="250px" position="absolute" left="800px" right="100px" top="200px">
+                     <Image src={background} alt="background" />
+             </Box>
+
+             <Text color="primary.100" position="absolute" fontSize="90px" fontFamily="saira" fontWeight="bold" left="100px" top="270px" >
+                Blogify...
+             </Text> 
+             <Box as="div" maxWidth="360px" position="absolute" left="100px" top="390px">
+             <Text color="primary.100" textAlign="left" lineHeight="10" fontSize="30px" fontFamily="saira" fontWeight="semibold" left="100px" top="270px" >
+                The best platform to share blogs and articles with large community 
+             </Text> 
+             </Box>
+
+        </Box>
     )
 }
 
